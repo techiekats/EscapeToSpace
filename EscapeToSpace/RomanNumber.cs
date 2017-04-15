2bd80eaa552c05f6e44166ef1b9b976bc390ab06
techiekats <kshah@xceligent.com>
2017-04-15 18:08:33 -0500

    successful parse of int

--- EscapeToSpace/RomanNumber.cs
+++ EscapeToSpace/RomanNumber.cs
@@ -49,13 +49,46 @@ namespace EscapeToSpace
             }
             if (valid)
             {
-                //check max repetitiona of I,X, C, M
+                //check max repetitions of I,X, C, M
                 valid = !(romanDigits.Count(c=> c=='I') > 3 || romanDigits.Count(c => c == 'X') > 3 || romanDigits.Count(c => c == 'C') > 3 || romanDigits.Count(c => c == 'M') > 3);
                 reasonIfInvalid = "Characters I,X,C,M can appear only thrice in a continuous sequence";
             }
             if (valid)
             {
                 //check sequence
+                int[] digits = GetIntForEachCharacter(romanDigits);
+                for (int i = 0; i < digits.Length - 1 && valid; i++)
+                {
+                    int current = digits[i];
+                    int next = digits[i + 1];
+                    int previous = (i == 0 ? int.MaxValue : digits[i-1]);
+                    if (next > current)
+                    {
+                        if (current == Symbols.V || current == Symbols.L || current == Symbols.D)
+                        {
+                            valid = false; //V, L and D can never be subtracted
+                        }
+                        /*"I" can be subtracted from "V" and "X" only. "X" can be subtracted from "L" and "C" only. "C" can be subtracted from
+"D" and "M" only. "V", "L", and "D" can never be subtracted*/
+                        else if (previous <= current && current < next)
+                        {
+                            valid = false;
+                        }
+                        else if (current == Symbols.I && !(next == Symbols.V || next == Symbols.X))
+                        {
+                            valid = false;
+                        }
+                        else if (current == Symbols.X && !(next == Symbols.L || next == Symbols.C))
+                        {
+                            valid = false;
+                        }
+                        else if (current == Symbols.C && !(next == Symbols.D || next == Symbols.M))
+                        {
+                            valid = false;
+                        }
+                    }
+                }
+                reasonIfInvalid = "Invalid number sequence";
             }
             reasonIfInvalid = valid ? string.Empty : reasonIfInvalid;
             return valid;
@@ -63,7 +96,56 @@ namespace EscapeToSpace
         private static int GetIntegerValue (string romanDigits)
         {
             int arabicNumber = 0;
+            int[] digits = GetIntForEachCharacter(romanDigits);
+            for (int i=0; i< digits.Length; i++)
+            {
+                int current = digits[i];
+                int next = (i == digits.Length - 1 ? int.MinValue : digits[i + 1]);
+                if (next > current)
+                {
+                    arabicNumber -= digits[i];
+                }
+                else
+                {
+                    arabicNumber += digits[i];
+                }
+            }
             return arabicNumber;
+        }
+        private static int[] GetIntForEachCharacter (string romanDigits)
+        {
+            int[] numbers = new int[romanDigits.Length];
+            for (int i = 0; i < romanDigits.Length; i++)
+            {
+                char c = romanDigits[i];
+                switch (c)
+                {
+                    case 'I':
+                        numbers[i] = Symbols.I;
+                        break;
+                    case 'V':
+                        numbers[i] = Symbols.V;
+                        break;
+                    case 'X':
+                        numbers[i] = Symbols.X;
+                        break;
+                    case 'L':
+                        numbers[i] = Symbols.L;
+                        break;
+                    case 'C':
+                        numbers[i] = Symbols.C;
+                        break;
+                    case 'D':
+                        numbers[i] = Symbols.D;
+                        break;
+                    case 'M':
+                        numbers[i] = Symbols.M;
+                        break;
+                    default:
+                        throw new InvalidCastException();                
+                }
+            }
+            return numbers;
         }
     }
 }
--- EscapeToSpaceTests/RomanNumberTests.cs
+++ EscapeToSpaceTests/RomanNumberTests.cs
@@ -74,5 +74,55 @@ namespace EscapeToSpace.Tests
                 }
             }
         }
+        [TestMethod()]
+        public void ParseTestForInvalidCharacterSequence()
+        {//"I" can be subtracted from "V" and "X" only. "X" can be subtracted from "L" and "C" only. "C" can be subtracted from
+          //  "D" and "M" only. "V", "L", and "D" can never be subtracted.
+            List<string> invalidNumbers = new List<string>();
+            invalidNumbers.Add("IC");//I can be subtracted only from V and X
+            invalidNumbers.Add("IM"); //I can be subtracted only from V and X
+            invalidNumbers.Add("ID");
+            invalidNumbers.Add("IL");
+            invalidNumbers.Add("MXM"); //X can be subtracted from L and C only; 
+            invalidNumbers.Add("MVX"); //V must never be subtracted
+            invalidNumbers.Add("MVC");
+            invalidNumbers.Add("MVM");
+            invalidNumbers.Add("MLC"); //L must never be subtracted
+            invalidNumbers.Add("MLM");
+            invalidNumbers.Add("MDM"); //D must never be subtracted
+            invalidNumbers.Add("IIV"); //only 1 small value must be subtracted
+            invalidNumbers.Add("IVX");
+            foreach (var t in invalidNumbers)
+            {
+                try
+                {
+                    RomanNumber.Parse(t);
+                    Assert.Fail(); //execution reaching this line implies successful parse.
+                }
+                catch (Exception exception)
+                {
+                    Assert.AreEqual(typeof(FormatException), exception.GetType());
+                    Assert.AreEqual("Invalid number sequence", exception.Message);
+                }
+            }
+        }
+        [TestMethod()]
+        public void ParseValidNumbers ()
+        {
+            List<Tuple<string, int>> validRomanNumbers = new List<Tuple<string, int>>();
+            validRomanNumbers.Add(new Tuple<string, int>("mmvi", 2006));
+            validRomanNumbers.Add(new Tuple<string, int>("iv", 4));
+            validRomanNumbers.Add(new Tuple<string, int>("x", 10));
+            validRomanNumbers.Add(new Tuple<string, int>("cm", 900));
+            validRomanNumbers.Add(new Tuple<string, int>("mm", 2000));
+            validRomanNumbers.Add(new Tuple<string, int>("mcmxcix", 1999));
+            validRomanNumbers.Add(new Tuple<string, int>("cxlviii", 148));
+            validRomanNumbers.Add(new Tuple<string, int>("III", 3));
+            validRomanNumbers.Add(new Tuple<string, int>("clxxxvi", 186));
+            foreach (var t in validRomanNumbers)
+            {
+                Assert.AreEqual(t.Item2, RomanNumber.Parse(t.Item1));
+            }
+        }
     }
 }
