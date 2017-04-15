c296a65578fc54cf188abed4dd71835467861bd0
techiekats <kshah@xceligent.com>
2017-04-15 17:08:09 -0500

    initial code for parsing

---
+++ EscapeToSpace/RomanNumber.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscapeToSpace
+{
+    public class RomanNumber
+    {
+        private struct Symbols
+        {
+            public const int I = 1;
+            public const int V = 5;
+            public const int X = 10;
+            public const int L = 50;
+            public const int C = 100;
+            public const int D = 500;
+            public const int M = 1000;
+        }
+        private RomanNumber ()
+        {
+            //never allow direct instantiation so that invalid instances never exist
+        }
+        public static int Parse (string romanDigits)
+        {
+            romanDigits = romanDigits.ToUpper();
+            string reasonIfInvalid;
+            if (Valid(romanDigits, out reasonIfInvalid))
+            {
+                return GetIntegerValue(romanDigits);
+            }
+            else
+            {
+                throw new FormatException(reasonIfInvalid);
+            }
+        }
+        private static bool Valid (string romanDigits, out string reasonIfInvalid)
+        {
+            bool valid = true;
+            reasonIfInvalid = "Characters other than I, V, X, C, D, M are not allowed";
+            //test if any character is incorrect
+            valid = romanDigits.All(c => (c == 'I' || c == 'V' || c == 'X' || c == 'L' || c == 'C' || c == 'D' || c == 'M'));
+            if (valid)
+            {
+                //check max occurrences of DLV
+                valid = !(romanDigits.Count(c=> c == 'D') > 1 || romanDigits.Count(c => c == 'L') > 1 || romanDigits.Count(c => c== 'V') > 1);
+                reasonIfInvalid = "Characters D,L,V must not be repeated";
+            }
+            if (valid)
+            {
+                //check max repetitiona of I,X, C, M
+                valid = !(romanDigits.Count(c=> c=='I') > 3 || romanDigits.Count(c => c == 'X') > 3 || romanDigits.Count(c => c == 'C') > 3 || romanDigits.Count(c => c == 'M') > 3);
+                reasonIfInvalid = "Characters I,X,C,M can appear only thrice in a continuous sequence";
+            }
+            if (valid)
+            {
+                //check sequence
+            }
+            reasonIfInvalid = valid ? string.Empty : reasonIfInvalid;
+            return valid;
+        }
+        private static int GetIntegerValue (string romanDigits)
+        {
+            int arabicNumber = 0;
+            return arabicNumber;
+        }
+    }
+}
---
+++ EscapeToSpaceTests/RomanNumberTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EscapeToSpace;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscapeToSpace.Tests
+{
+    [TestClass()]
+    public class RomanNumberTests
+    {
+        [TestMethod()]
+        public void ParseTestForInvalidCharacter()
+        {
+            List<string> invalidNumbers = new List<string>();
+            invalidNumbers.Add("In"); //invalid character 'n'
+            foreach (var t in invalidNumbers)
+            {
+                try
+                {
+                    RomanNumber.Parse(t);
+                    Assert.Fail(); //execution reaching this line implies successful parse.
+                }
+                catch (Exception exception)
+                {
+                    Assert.AreEqual(typeof(FormatException), exception.GetType());
+                    Assert.AreEqual("Characters other than I, V, X, C, D, M are not allowed", exception.Message);
+                }
+            }
+        }
+        [TestMethod()]
+        public void ParseTestForInvalidCharacterRepeated()
+        {
+            List<string> invalidNumbers = new List<string>();
+            invalidNumbers.Add("DDC"); //D can never be repeated
+            invalidNumbers.Add("LLVM"); //L can never be repeated
+            invalidNumbers.Add("VIV"); //V must never be repeated
+            foreach (var t in invalidNumbers)
+            {
+                try
+                {
+                    RomanNumber.Parse(t);
+                    Assert.Fail(); //execution reaching this line implies successful parse.
+                }
+                catch (Exception exception)
+                {
+                    Assert.AreEqual(typeof(FormatException), exception.GetType());
+                    Assert.AreEqual("Characters D,L,V must not be repeated", exception.Message);
+                }
+            }
+        }
+        
+        [TestMethod()]
+        public void ParseTestForInvalidCharacterRepetitions()
+        {
+            List<string> invalidNumbers = new List<string>();
+            invalidNumbers.Add("XXXXV");
+            invalidNumbers.Add("MCCCCD");
+            invalidNumbers.Add("MMMMV");
+            invalidNumbers.Add("IIII"); //I repeated 4 times
+            foreach (var t in invalidNumbers)
+            {
+                try
+                {
+                    RomanNumber.Parse(t);
+                    Assert.Fail(); //execution reaching this line implies successful parse.
+                }
+                catch (Exception exception)
+                {
+                    Assert.AreEqual(typeof(FormatException), exception.GetType());
+                    Assert.AreEqual("Characters I,X,C,M can appear only thrice in a continuous sequence", exception.Message);
+                }
+            }
+        }
+    }
+}
